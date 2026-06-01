4e8ec0db28cc4c2fc227a028fb10a38c616b0307
Poppo <poppo@batams-MacBook-Pro.local>
2026-06-01 10:57:08 +0700

    update user

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
             args: "start",
             // Optional: Launches instances across all CPU cores for speed
             watch: true, // Only use this if you want auto-restarts on code updates
-            ignore_watch: ["node_modules", "public/uploads"], // Prevents upload loops
+            ignore_watch: ["node_modules", "/public/uploads", "/uploads"], // Prevents upload loops
             env: {
                 NODE_ENV: "production"
             }
